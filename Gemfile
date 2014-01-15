9bf5d75941989cc26e3359a6f1f6a23fc266084f
Andre Smith <andrepsmithjr@gmail.com>
2014-01-14 16:15:41 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :doc do.
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '65a3669825e21bbb5a26dc2b5eef495ef99ef8ed057bd43d402a0e4b33838d6edb573f7fa96233dd5b76f4a28519b182105461c11ce3192a209463c94cabf404'
