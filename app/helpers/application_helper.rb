8de53be1b0526ced0f0264fce6ce13bc99b746a0
Andre Smith <lordsilent@gmail.com>
2014-01-20 23:33:03 -0800

    Add ability to add new projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when :notice then "success"
+      when :success then "success"
+      when :error then "alert"
+      when :alert then "alert"
+    end
+  end
 end
--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -6,12 +6,12 @@ feature "Creating A Project" do
 
     click_on "New Project"
 
-    fill_in 'Title', :with => 'Code Fellows Portfolio'
-    fill_in 'Technologies used', :with => "Rails, Ruby, Zurb,"
+    fill_in 'Name', :with => 'Code Fellows Portfolio'
+    fill_in 'Technologies used', :with => "Rails, Ruby, Zurb"
 
     click_on 'Create Project'
 
     page.text.must_include "Project has been created"
-    page.text.must_include "Test Project"
+    page.text.must_include "Code Fellows Portfolio"
   end
 end
