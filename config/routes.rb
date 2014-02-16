a016fb41609fe5ff943db9cb1b555f83dc3d5791
Andre Smith <andrepsmithjr@gmail.com>
2014-02-15 17:39:45 -0800

    Add I18n support, start Japanese translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Portfolio::Application.routes.draw do
 
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
-  resources :projects
 
-  resources :posts do
-    resources :comments
+  scope ":locale" do
+    resources :projects
+
+    resources :posts do
+      resources :comments
+    end
   end
-
 
   root to:"welcome#index"
 end
