955b632380811caec6e6bdf43c2f460c74a5b561
Andre Smith <andrepsmithjr@gmail.com>
2014-02-21 15:07:16 -0800

    Finish polymorphic comments

--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -23,8 +23,7 @@ feature "Creating A Project" do
     click_on "Create Project"
     #Then the form should be displayed again with an error message
     current_path.must_match /projects$/
-    page.text.must_include "Project could not be saved"
-    page.text.must_include "Name is too short"
+    page.text.must_include "Name is too short (minimum is 4 characters)"
     page.text.must_include "Technologies used can't be blank"
   end
 end
