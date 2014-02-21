d7551e1982a12b0205b1025b0b021b17cf986104
Andre Smith <andrepsmithjr@gmail.com>
2014-02-20 19:44:51 -0800

    Remove I18n due to interfering with poly comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,5 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  before_action :set_locale
 
   protect_from_forgery with: :exception
-
-  private
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def default_url_options(options={})
-    { :locale => I18n.locale }
-  end
 end
